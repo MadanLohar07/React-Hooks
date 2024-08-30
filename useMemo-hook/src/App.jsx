75e8ec6702e44104f46b3c652212ee2b72773339
MadanLohar07 <madanpanchal211@gmail.com>
2024-08-31 01:16:35 +0530

    useCallback hook

--- useMemo-hook/src/App.jsx
+++ useMemo-hook/src/App.jsx
@@ -1,13 +1,8 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
 import "./App.css";
 import UseMemoHook from "./Components/UseMemoHook";
 import FilteredList from "./Components/FilteredList";
 
 function App() {
-  const [count, setCount] = useState(0);
-
   return (
     <>
       {/* <UseMemoHook /> */}
