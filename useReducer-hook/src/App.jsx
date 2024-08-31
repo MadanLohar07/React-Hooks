1061caa333b9d7a95bebd7194ab7eb7a13428742
MadanLohar07 <madanpanchal211@gmail.com>
2024-08-31 11:28:17 +0530

    useReducer hook

--- useReducer-hook/src/App.jsx
+++ useReducer-hook/src/App.jsx
@@ -1,61 +1,10 @@
-import { useReducer } from "react";
 import "./App.css";
+import Counter from "./Components/Counter";
 
 function App() {
-  const initialState = {
-    count: 0,
-    error: null,
-  };
-
-  const reducer = (initialState, action) => {
-    switch (action) {
-      case "increment": {
-        const newCount = initialState.count + 1;
-        const hasError = newCount > 5;
-        return {
-          ...initialState,
-          count: hasError ? initialState.count : newCount,
-          error: hasError ? "Maximum reached" : null,
-        };
-      }
-
-      case "decrement": {
-        const newCount = initialState.count - 1;
-        const hasError = newCount < 0;
-        return {
-          ...initialState,
-          count: hasError ? initialState.count : newCount,
-          error: hasError ? "Minimum reached" : null,
-        };
-      }
-
-      default: {
-        return initialState;
-      }
-    }
-  };
-
-  const [state, dispatch] = useReducer(reducer, initialState);
-
   return (
     <>
-      <h2>Counter using useReducer hook</h2>
-      <h3> Count : {state.count} </h3>
-      <p>{state.error}</p>
-      <button
-        onClick={() => {
-          dispatch("increment");
-        }}
-      >
-        Increment
-      </button>
-      <button
-        onClick={() => {
-          dispatch("decrement");
-        }}
-      >
-        Decrement
-      </button>
+      <Counter />
     </>
   );
 }
